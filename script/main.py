624c749cfb5e10e4ecf37ba6a4d3d45f4664b809
thibaut <thibaut.gonnin@gmail.com>
2018-12-10 10:54:13 +0100

    new main.py for simulation

--- script/main.py
+++ script/main.py
@@ -5,12 +5,16 @@ from scxml.pyscxml import StateMachine
 path_to_file = sys.argv[1]
 list_element = sys.argv[2]
 
-events = list_element.split(",")
+file_event = open(list_element,"r")
+events = file_event.read().split("\n")
+
+file_event.close()
 
 print("******Starting Simulation******")
 sm = StateMachine(path_to_file)
 sm.start_threaded()
 
+element = events.pop()
 while(len(events) != 0):
 	element = events.pop()
 	print("** Injecting event:" + element + " **")
